47a4fbe09fcec66cacfc043d30d7b97ee194647e
Olof Haglund <olof@olofhaglund.name>
2022-09-19 20:54:44 +0200

    Cats are also cute

--- main.py
+++ main.py
@@ -2,33 +2,43 @@
 
 import os
 import datetime
+import time
 
 from dataclasses import dataclass
 
 def print_end(start, string):
-    print(f"Connectivity is {string}")
-    print(f"Start: {start}")
-    end = datetime.datetime.now()
-    print(f"End: {end}")
-    print(f"Duration: {end - start} ")
+    with open("output.txt", "a") as file:
+        file.write(f"Connectivity is {string}")
+        file.write(f"Start: {start}")
+        end = datetime.datetime.now()
+        file.write(f"End: {end}")
+        file.write(f"Duration: {end - start} ")
 
 def main():
     up = True
     start = datetime.datetime.now()
+    sleep = 10
 
     hostname = "olofhaglund.name"
-    response = os.system(f"ping -c 1 {hostname}")
 
-    if response == 0:
-        if !uptime.up:
-            print_end(start, "up")
-            start = datetime.now()
-            up = True
-    else:
-        if uptime.up:
-            print_end(start, "lost")
-            start = datetime.now()
-            up = False
+    with open("output.txt", "w") as file:
+        file.write(f"Monitoring started {datetime.datetime.now()} with pings toward {hostname} every {sleep} seconds")
+
+
+    while True:
+        response = os.system(f"ping -c 1 {hostname}")
+
+        if response == 0:
+            if not up:
+                print_end(start, "up")
+                start = datetime.now()
+                up = True
+        else:
+            if up:
+                print_end(start, "lost")
+                start = datetime.now()
+                up = False
+        time.sleep(sleep)
 
 if __name__ == '__main__':
     main()
